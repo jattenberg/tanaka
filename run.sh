c60a470d55d15fd084ddb08147da15958b9b42ac
Josh Attenberg <josh.attenberg@gmail.com>
2023-01-10 21:02:38 -0500

    correct url

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 QUOTE=$(curl $BS_URL | shuf -n 1)
 
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$QUOTE"'"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$QUOTE"'"}' $SLACK_WEBHOOK_URL
 
