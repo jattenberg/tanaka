9f49336fe50cee7269efa178fb045283d4282a6e
Josh Attenberg <josh.attenberg@gmail.com>
2023-01-11 12:20:33 -0500

    avoiding one word lines

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-QUOTE=$(curl $BS_URL | shuf -n 1)
+QUOTE=$(curl $BS_URL |  grep -P '\w+\s+\w+' | shuf -n 1)
 
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$QUOTE"'"}' $SLACK_WEBHOOK_URL
