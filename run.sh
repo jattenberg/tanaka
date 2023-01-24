bbdc8344bb8aa905acce02cdd3b7e3b311101273
Josh Attenberg <josh.attenberg@gmail.com>
2023-01-24 06:57:11 -0500

    trying this

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-QUOTE=$(curl $BS_URL |  grep -P '\w+\s+\w+' | shuf -n 1)
+QUOTE=$(cat bs.txt |  grep -P '\w+\s+\w+' | shuf -n 1)
 
 echo $(date)" => "$QUOTE
 curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$QUOTE"'"}' $SLACK_WEBHOOK_URL
